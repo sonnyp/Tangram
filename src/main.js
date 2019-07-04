acbdbf79c6dc214404e7cf8488d78262c494c2ba
Sonny Piers <sonny@fastmail.net>
2019-07-04 14:24:28 +0200

    created project with gnome-builder

---
+++ src/main.js
@@ -0,0 +1,47 @@
+/* main.js
+ *
+ * Copyright 2019 Sonny Piers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+pkg.initGettext();
+pkg.initFormat();
+pkg.require({
+  'Gio': '2.0',
+  'Gtk': '3.0'
+});
+
+const { Gio, Gtk } = imports.gi;
+
+const { GigagramWindow } = imports.window;
+
+function main(argv) {
+    const application = new Gtk.Application({
+        application_id: 're.sonny.gigagram',
+        flags: Gio.ApplicationFlags.FLAGS_NONE,
+    });
+
+    application.connect('activate', app => {
+        let activeWindow = app.activeWindow;
+        
+        if (!activeWindow) {
+            activeWindow = new GigagramWindow(app);
+        }
+
+        activeWindow.present();
+    });
+
+    return application.run(argv);
+}
---
+++ src/window.js
@@ -0,0 +1,30 @@
+/* window.js
+ *
+ * Copyright 2019 Sonny Piers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const { GObject, Gtk } = imports.gi;
+
+var GigagramWindow = GObject.registerClass({
+    GTypeName: 'GigagramWindow',
+    Template: 'resource:///re/sonny/gigagram/window.ui',
+    InternalChildren: ['label']
+}, class GigagramWindow extends Gtk.ApplicationWindow {
+    _init(application) {
+        super._init({ application });
+    }
+});
+
