ffcd548f6968aee8c10d3f93d91e681f37508d6c
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2019-08-02 09:32:23 -0500

    Headerbar buttonBox improvements

--- src/header.js
+++ src/header.js
@@ -11,7 +11,6 @@
     Builder,
     IconSize,
     Image,
-    STYLE_CLASS_LINKED,
   } = imports.gi.Gtk;
 
   function Menu() {
@@ -53,24 +52,13 @@
       spacing: 6,
     });
     stack.add_named(buttonBox, "tabs");
-
-    const navigationButtons = new Box({ spacing: 0 });
-    navigationButtons.get_style_context().add_class(STYLE_CLASS_LINKED);
-    buttonBox.add(navigationButtons);
-
-    const backButton = Button.new_from_icon_name(
-      "go-previous-symbolic",
+    const addTabButton = Button.new_from_icon_name(
+      "tab-new-symbolic",
       IconSize.BUTTON
     );
-    navigationButtons.add(backButton);
-    backButton.connect("clicked", onGoBack);
-
-    const forwardButton = Button.new_from_icon_name(
-      "go-next-symbolic",
-      IconSize.BUTTON
-    );
-    navigationButtons.add(forwardButton);
-    forwardButton.connect("clicked", onGoForward);
+    buttonBox.add(addTabButton);
+    addTabButton.set_always_show_image(true);
+    addTabButton.connect("clicked", onAddTab);
 
     const reloadButton = Button.new_from_icon_name(
       "view-refresh-symbolic",
@@ -79,13 +67,23 @@
     buttonBox.add(reloadButton);
     reloadButton.connect("clicked", onReload);
 
-    const addTabButton = Button.new_from_icon_name(
-      "tab-new-symbolic",
+    const navButtonBox = new Box();
+    buttonBox.add(navButtonBox);
+    navButtonBox.get_style_context().add_class("linked");
+
+    const backButton = Button.new_from_icon_name(
+      "go-previous-symbolic",
       IconSize.BUTTON
     );
-    buttonBox.add(addTabButton);
-    addTabButton.set_always_show_image(true);
-    addTabButton.connect("clicked", onAddTab);
+    navButtonBox.add(backButton);
+    backButton.connect("clicked", onGoBack);
+
+    const forwardButton = Button.new_from_icon_name(
+      "go-next-symbolic",
+      IconSize.BUTTON
+    );
+    navButtonBox.add(forwardButton);
+    forwardButton.connect("clicked", onGoForward);
 
     const serviceBox = new Box();
     const cancelButton = Button.new_from_icon_name(
