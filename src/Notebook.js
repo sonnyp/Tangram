65d8a333ae6532d17b1f448fdec28f2828474155
Sonny Piers <sonny@fastmail.net>
2019-08-10 15:54:20 +0200

    move shortcuts, actions, notebook, application to their own files

---
+++ src/Notebook.js
@@ -0,0 +1,105 @@
+const { build_filenamev } = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
+const { SettingsBindFlags, File, FileCopyFlags } = imports.gi.Gio;
+
+const { Settings, connect } = imports.util;
+const { state } = imports.state;
+const {
+  createApplication,
+  launchApplication,
+  buildApplicationId,
+} = imports.applicationDialog;
+const instances = imports.instances;
+const { data_dir } = imports.env;
+
+this.detachTab = detachTab;
+function detachTab({ instance_id, notebook, settings }) {
+  const instance = instances.get(instance_id);
+  const { name } = instance;
+  const id = buildApplicationId(name);
+
+  let icon = instance.getIconForDisplay();
+  if (icon && icon.startsWith("resource://")) {
+    const file = File.new_for_uri(icon);
+    const dest = build_filenamev([data_dir, `${id}.svg`]);
+    file.copy(File.new_for_path(dest), FileCopyFlags.OVERWRITE, null, null);
+    icon = dest;
+  }
+
+  let desktopFilePath;
+  try {
+    desktopFilePath = createApplication({ name, icon, id }).desktopFilePath;
+  } catch (err) {
+    logError(err);
+    // TODO show error
+    return;
+  }
+
+  const newAppSettings = new Settings({
+    schema_id: "re.sonny.Tangram",
+    path: `/re/sonny/Tangram/applications/${id}/`,
+  });
+  instances.attach(newAppSettings, instance.id);
+
+  try {
+    launchApplication(desktopFilePath);
+  } catch (err) {
+    logError(err);
+    // todo show error and cleanup
+    return;
+  }
+
+  const idx = instances.detach(settings, instance.id);
+
+  const page = notebook.get_nth_page(idx);
+  notebook.detach_tab(page);
+  const label = notebook.get_tab_label(page);
+  if (label) label.destroy();
+  page.destroy();
+}
+
+this.Notebook = function Notebook({ profile, settings }) {
+  // https://gjs-docs.gnome.org/gtk30~3.24.8/gtk.notebook
+  const notebook = new Gtk.Notebook({ scrollable: true });
+  this.notebook = notebook;
+  // Tab bar only hides on custom applications
+  if (profile.id) {
+    state.bind(
+      "instances",
+      notebook,
+      "show_tabs",
+      instances => instances.length > 1
+    );
+  }
+  notebook.connect("switch-page", (self, webview) => {
+    state.set({ webview });
+  });
+  notebook.set_group_name("tabs");
+  notebook.show_all();
+  connect(
+    notebook,
+    {
+      ["page-reordered"]() {
+        const number_of_pages = notebook.get_n_pages();
+
+        const instances = settings.get_strv("instances");
+        const reordered = [];
+
+        for (let i = 0; i < number_of_pages; i++) {
+          const id = notebook.get_nth_page(i).instance_id;
+          if (!instances.includes(id)) continue;
+          reordered.push(id);
+        }
+
+        settings.set_strv("instances", reordered);
+      },
+    }
+  );
+  settings.bind("tabs-position", notebook, "tab_pos", SettingsBindFlags.GET);
+
+  notebook.connect("create-window", (self, { instance_id } /*_x, _y */) => {
+    detachTab({ instance_id, settings, notebook });
+  });
+
+  return notebook;
+};
---
+++ src/Shortcuts.js
@@ -0,0 +1,139 @@
+const { VariantType } = imports.gi.GLib;
+const { AccelGroup, AccelFlags, accelerator_parse } = imports.gi.Gtk;
+const { SimpleAction } = imports.gi.Gio;
+const { ModifierType, keyval_name } = imports.gi.Gdk;
+
+function nextPage(notebook) {
+  if (notebook.page === notebook.get_n_pages() - 1) {
+    notebook.page = 0;
+  } else {
+    notebook.next_page();
+  }
+}
+function prevPage(notebook) {
+  if (notebook.page === 0) {
+    notebook.page = notebook.get_n_pages() - 1;
+  } else {
+    notebook.prev_page();
+  }
+}
+
+this.Shortcuts = function Shortcuts({
+  window,
+  application,
+  notebook,
+  addressBar,
+  onStopLoading,
+  onReload,
+  onGoBack,
+  onGoForward,
+  onShowInspector,
+}) {
+  const nthTab = new SimpleAction({
+    name: "nth-tab",
+    parameter_type: VariantType.new("i"),
+  });
+  for (let i = 1; i < 10; i++) {
+    application.set_accels_for_action(`app.nth-tab(${i})`, [`<Alt>${i}`]);
+  }
+  nthTab.connect("activate", (self, parameters) => {
+    const idx = parameters.deep_unpack();
+    notebook.page = idx - 1;
+  });
+  application.add_action(nthTab);
+
+  // https://gjs-docs.gnome.org/gtk30~3.24.8/gtk.accelgroup
+  const accelGroup = new AccelGroup();
+  window.add_accel_group(accelGroup);
+  const shortcuts = [
+    [["Escape"], onStopLoading],
+    [["<Primary>R", "F5"], onReload],
+    [["<Primary><Shift>R", "<Shift>F5"], () => onReload(true)],
+    [
+      ["<Alt>Left"],
+      () => {
+        onGoBack();
+        // prevents default notebook behavior
+        return true;
+      },
+    ],
+    [
+      ["<Alt>Right"],
+      () => {
+        onGoForward();
+        // prevents default notebook behavior
+        return true;
+      },
+    ],
+    [["<Primary><Shift>I"], onShowInspector],
+    [
+      ["<Primary>L"],
+      () => {
+        addressBar.grab_focus();
+      },
+    ],
+  ];
+  shortcuts.forEach(([accels, fn]) => {
+    accels.forEach(accel => {
+      const [accelerator_key, accelerator_mods] = accelerator_parse(accel);
+      accelGroup.connect(
+        accelerator_key,
+        accelerator_mods,
+        AccelFlags.VISIBLE,
+        fn
+      );
+    });
+  });
+
+  window.connect("key-press-event", (self, event) => {
+    const [, modifier] = event.get_state();
+    const [, keyval] = event.get_keyval();
+    const name = keyval_name(keyval);
+
+    // CTRL + Page_Down
+    if (name === "Page_Down" && modifier === ModifierType.CONTROL_MASK) {
+      nextPage(notebook);
+      return true;
+    }
+    // CTRL + Tab
+    if (
+      ["Tab", "ISO_Left_Tab", "KP_Tab"].includes(name) &&
+      modifier === ModifierType.CONTROL_MASK
+    ) {
+      nextPage(notebook);
+      return true;
+    }
+
+    // CTRL + Page_Up
+    if (name === "Page_Up" && modifier === ModifierType.CONTROL_MASK) {
+      prevPage(notebook);
+      return true;
+    }
+    // CTRL+SHIFT + Tab
+    if (
+      ["Tab", "ISO_Left_Tab", "KP_Tab"].includes(name) &&
+      modifier === ModifierType.CONTROL_MASK + 1
+    ) {
+      prevPage(notebook);
+      return true;
+    }
+
+    // CTRL+SHIFT + PageUp
+    if (name === "Page_Up" && modifier === ModifierType.CONTROL_MASK + 1) {
+      const { page } = notebook;
+      if (page === 0) return true;
+      notebook.reorder_child(notebook.get_nth_page(page), page - 1);
+      return true;
+    }
+
+    // CTRL+SHIFT + PageDown
+    if (name === "Page_Down" && modifier === ModifierType.CONTROL_MASK + 1) {
+      const { page } = notebook;
+      if (page === notebook.get_n_pages() - 1) return true;
+      notebook.reorder_child(notebook.get_nth_page(page), page + 1);
+      return true;
+    }
+
+    return false;
+  });
+};
