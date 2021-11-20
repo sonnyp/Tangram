dd78c7aed73cdc33d9a89071c18dc687a17f5363
Sonny Piers <sonny@fastmail.net>
2021-11-20 13:47:21 +0100

    Set notification priority per tab (#160)

--- src/util.js
+++ src/util.js
@@ -1,5 +1,7 @@
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
+import Gtk from "gi://Gtk";
+import Gdk from "gi://Gdk";
 
 const { Settings: GioSettings, keyfile_settings_backend_new } = Gio;
 const { KeyFile, KEY_FILE_DESKTOP_GROUP } = GLib;
@@ -82,4 +84,28 @@ export function observeProperty(GObject, name, fn) {
   GObject.connect(`notify::${name}`, () => {
     return fn(GObject[name]);
   });
+}
+
+export function logEnum(obj, value) {
+  log(
+    Object.entries(obj).find(([, v]) => {
+      return v === value;
+    })[0],
+  );
+}
+
+export function relativePath(path) {
+  const [filename] = GLib.filename_from_uri(import.meta.url);
+  const dirname = GLib.path_get_dirname(filename);
+  return GLib.canonicalize_filename(path, dirname);
+}
+
+export function loadStyleSheet(path) {
+  const provider = new Gtk.CssProvider();
+  provider.load_from_path(path);
+  Gtk.StyleContext.add_provider_for_display(
+    Gdk.Display.get_default(),
+    provider,
+    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+  );
 }
