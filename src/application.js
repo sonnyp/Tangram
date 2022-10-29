2d3b782ac925596e42ba76dfdde086b87af73896
Sonny Piers <sonny@fastmail.net>
2022-10-29 14:48:58 +0200

    Fix tests

--- src/application.js
+++ src/application.js
@@ -3,6 +3,8 @@ import Adw from "gi://Adw";
 import Window from "./window.js";
 import state from "./state.js";
 
+import "./blank.html";
+
 const application = new Adw.Application({
   application_id: "re.sonny.Tangram",
 });
